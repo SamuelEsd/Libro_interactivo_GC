7ded1329edb94401002e34be862dc7dad187728f
SamuelEsd <mr_s_p@ciencias.unam.mx>
2022-05-03 18:05:55 -0500

    CHA-02 Adding steps to Jarvis March interactive resource
    Adding animation representing the searching of the next point to be added to the convex hull.
    
    Modifying definitions of convex hull.

--- Interactivos/capitulo_2/utils/Display_JM.js
+++ Interactivos/capitulo_2/utils/Display_JM.js
@@ -1,101 +1,118 @@
-let points = []
-let convexHull = []
+let points = [];
+let convexHull = [];
+let currPoint = null;
+let prevVertex = null;
 
-let take_step = false
-let completed = false
-let button
+let take_step = false;
+let completed = false;
+let button;
 
 function setup() {
-  CUSTOM_WIDTH = windowWidth - 30
-  CUSTOM_HEIGHT =  windowHeight - 30
-  createCanvas(CUSTOM_WIDTH,CUSTOM_HEIGHT);
+  CUSTOM_WIDTH = windowWidth - 30;
+  CUSTOM_HEIGHT = windowHeight - 30;
+  createCanvas(CUSTOM_WIDTH, CUSTOM_HEIGHT);
   strokeWeight(POINT_SIZE);
   initializeButton();
 }
 
-function initializePoints(num_of_points){
-  startingP = createVector(WINDOW_BORDER, WINDOW_BORDER)
-  endingP = createVector(CUSTOM_WIDTH - WINDOW_BORDER, CUSTOM_HEIGHT - WINDOW_BORDER)
-  points = getRandomPointsInArea(num_of_points,startingP,endingP)
+function initializePoints(num_of_points) {
+  startingP = createVector(WINDOW_BORDER, WINDOW_BORDER);
+  endingP = createVector(
+    CUSTOM_WIDTH - WINDOW_BORDER,
+    CUSTOM_HEIGHT - WINDOW_BORDER
+  );
+  points = getRandomPointsInArea(num_of_points, startingP, endingP);
 }
 
-function initializeButton(){
-  button = createButton('Siguiente');
+function initializeButton() {
+  button = createButton("Siguiente");
   button.position(CUSTOM_WIDTH - 100, CUSTOM_HEIGHT - 25);
   button.mousePressed(step);
 }
 
-function step(){
-  print("Step!")
-  take_step = true
-}
-
-
-function draw() {
+async function step() {
   // initialize alg
-  if(take_step && points.length == 0){
+  if (points.length == 0) {
     initializePoints(15);
-    take_step = false
   }
   // alg steps
-  if(take_step && !completed){
-    take_step = false
+  if (!completed) {
     // add fisrt point
-    if(convexHull.length == 0){
-      let minXPoint = points[0]
-      for(let point of points){
-        if(point.x < minXPoint.x || (point.x ==  minXPoint.x && point.y < minXPoint.y)){
-          minXPoint = point
+    if (convexHull.length == 0) {
+      let minXPoint = points[0];
+      for (let point of points) {
+        if (
+          point.x < minXPoint.x ||
+          (point.x == minXPoint.x && point.y < minXPoint.y)
+        ) {
+          minXPoint = point;
         }
       }
-      convexHull.push(minXPoint)
+      convexHull.push(minXPoint);
     }
     // add the next corresponding point to the convex hull
-    else{
-      let prevVertex = convexHull[convexHull.length - 1]
+    else {
+      prevVertex = convexHull[convexHull.length - 1];
       let rightmostVertex = null;
-      for(let point of points){
-        if(point == prevVertex) continue;
+      for (let point of points) {
+        currPoint = point
+        await sleep(50)
+        if (point == prevVertex) continue;
         // Take any point diferent to last vertex on hull
         // as rightmost
-        if(rightmostVertex == null){
+        if (rightmostVertex == null) {
           rightmostVertex = point;
           continue;
         }
-  
+
         let td = turnDirection(prevVertex, rightmostVertex, point);
         let isColinear = td == 0;
         let isLeftTurn = td == 1;
-        let pointIsFarder = distance(prevVertex, rightmostVertex) < distance(prevVertex, point);
+        let pointIsFarder =
+          distance(prevVertex, rightmostVertex) < distance(prevVertex, point);
         // If there's a point more to the right or colinear but farder
         // that will be our new rightmost
-        if ((isColinear && pointIsFarder) || isLeftTurn){
+        if ((isColinear && pointIsFarder) || isLeftTurn) {
           rightmostVertex = point;
         }
       }
-      
+
       // If we reach the start it's time to finish
-      if(rightmostVertex == convexHull[0]){
-        completed = true
+      if (rightmostVertex == convexHull[0]) {
+        completed = true;
+        currPoint = null;
+        prevVertex = null;
       }
-      // It not, add rightmost vertex to hull and take it as the new 
+      // It not, add rightmost vertex to hull and take it as the new
       // vertex to compare with
-      else{
+      else {
         convexHull.push(rightmostVertex);
         prevVertex = rightmostVertex;
+        currPoint = null;
+        prevVertex = null;
       }
     }
   }
   // reset alg
-  if(take_step && completed){
-    take_step = false
-    completed = false
-    convexHull = []
-    points = []
+  else {
+    completed = false;
+    convexHull = [];
+    points = [];
   }
-  
+}
+
+function sleep(millisecondsDuration) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, millisecondsDuration);
+  });
+}
+
+function draw() {
   background(0);
-  drawArrows(convexHull,completed);
-  drawPoints(points,256);
-  drawPoints(convexHull,'blue');
+  drawArrows(convexHull, completed);
+  drawPoints(points, 256);
+  drawPoints(convexHull, "blue");
+  if( currPoint != null){
+    drawArrow(prevVertex, currPoint, "white");
+  }
 }

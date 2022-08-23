df0191dc718d8a19ec7d780191ec3c883d944f70
SamuelEsd <mr_s_p@ciencias.unam.mx>
2022-08-22 19:54:18 -0500

    CHA-2 dynamic algorithm
    Adding image in order to explain where the tangents are on the dynamic algorithm.
    Adding interactive resource of the dynamic algorithm.

--- Interactivos/capitulo_2/utils/Display_Merge_Point.js
+++ Interactivos/capitulo_2/utils/Display_Merge_Point.js
@@ -9,16 +9,17 @@ let tangent_a = new PointSet([]);
 let tangent_b = new PointSet([]);
 
 let isRightTurn = null;
+let iterations = 0;
 let arrowColor = null;
-let checkLeft;
-let checkRight;
-let changesOnLeft;
-let changesOnRight;
+let lastTurn = null;
+let index_a = null;
+let index_b = null;
 
-let highest_intersection;
-let lowest_intersection;
+let reset = false;
 let num_of_points = 10;
-let displayTangents = false;
+
+let rightTurnColor;
+let leftTurnColor;
 
 // Vars used for the visualizations of steps
 let button;
@@ -37,6 +38,8 @@ function setup() {
   description = createElement("h3", "");
   inputDescription = createElement("h4", "");
   outputDescription = createElement("h4", "");
+  rightTurnColor = color(0, 0, 256, 180);
+  leftTurnColor = color(256, 256, 256, 180);
 
   resetAlgorithm();
 }
@@ -52,88 +55,161 @@ function resetAlgorithm() {
 
   convexHull = new PointSet(convexHullJM(pointsSet));
   convexHull.setIds();
+  removeExtras();
 
-  tangent_a.points = [];
-  tangent_b.points = [];
+  iterations = 0;
+  reset = false;
+  lastTurn = null;
+  index_a = null;
+  index_b = null;
 
-  description.html("Para iniciar la visualización y agregar un punto aleatorio presione el botón iniciar.");
+  description.html("Para iniciar la visualización presione el botón iniciar.");
   button.html("Iniciar");
 }
 
+function updateConvexHull() {
+  new_convexHull = [];
+  let addNode = true;
+  for (let i = 0; i < convexHull.size(); i++) {
+    if (i == index_b) {
+      addNode = true;
+    }
+    if (addNode) {
+      new_convexHull.push(convexHull.points[i]);
+    }
+    if (i == index_a) {
+      new_convexHull.push(p.points[0]);
+      addNode = false;
+    }
+  }
+  convexHull.points = new_convexHull;
+}
+
+function removeExtras(){
+  curr_edge.points = [];
+  p.points = [];
+  tangent_a.points = [];
+  tangent_b.points = [];
+  isRightTurn = null;
+  arrowColor = null;
+  iterations = 0;
+}
+
+function addRandomPoint() {
+  let new_random_point = getRandomPointsInArea(
+    1,
+    UP_LEFT_CORNER,
+    DOWN_RIGHT_CORNER
+  )[0];
+  new_random_point.z = convexHull.size() + 1;
+
+  p.push(new_random_point);
+  curr_index = 0;
+}
+
 async function step() {
-  // When we have the tangents we finish the algorithm
-  if (tangent_a.size() != 0 && tangent_b.size() != 0){
+  // When we have the tangents or we hav checked all the edges, we finish the algorithm
+  if ((tangent_a.size() != 0 && tangent_b.size() != 0) || (iterations > convexHull.size() + 1)) {
     // reset alg
     if (reset) {
       resetAlgorithm();
-      reset = false;
-    } else {
-      description.html("La tangente inferior ha sido encontrada.");
+      return;
+    }
+    // Finish convex hull
+    if (iterations > convexHull.size() + 1) {
+      description.html("Ya se revisaron todas las aristas y todas son vueltas izquierdas, entonces el punto estaba dentro del cierre convexo y este permanece igual.");
+    }
+    else {
+      description.html("Ya se encontraron las tangentes y se puede formar el nuevo cierre convexo.");
+      updateConvexHull();
+    }
+    if (convexHull.size() == 6){  
       button.html("Reiniciar");
       reset = true;
     }
+    else{
+      removeExtras();
+    }
   }
+  // start algorithm adding a random point
   else if (p.size() == 0) {
-    let new_random_point = getRandomPointsInArea(
-      1,
-      UP_LEFT_CORNER,
-      DOWN_RIGHT_CORNER
-    )[0];
-    new_random_point.z = convexHull.size() + 1;
-
-    p.push(new_random_point);
-    curr_index = 0;
-
+    addRandomPoint();
     description.html(
-      "Tomaremos los dos primeros puntos del cierre convexo actual, para calcular que tipo de vuelta se dará."
+      "Agregamos un punto aleatorio."
     );
     button.html("Siguiente");
-
   }
+  // Iterate over the next edge
   else if (curr_edge.size() == 0) {
     curr_edge.push(convexHull.points[curr_index]);
-    curr_index = (curr_index + 1)%convexHull.size();
+    curr_index = (curr_index + 1) % convexHull.size();
     curr_edge.push(convexHull.points[curr_index]);
-    inputDescription.html(convexHull);
+    iterations++;
 
     description.html(
-      "Tomamos los siguientes dos puntos del cierre convexo actual, para calcular que tipo de vuelta se dará."
+      "Tomaremos dos puntos del cierre convexo actual, para calcular que tipo de vuelta se dará."
     );
+    inputDescription.html(convexHull);
     button.html("Siguiente");
   }
-  // if tangent point has been modified in at least one side, we need to check
-  // if a lower tangen can be found.
+  // Check the direction of the turn of the point p and the current edge.
   else {
+    // We need to get the current turn direction
     if (isRightTurn === null) {
       isRightTurn = turnDirection(curr_edge.points[0], curr_edge.points[1], p.points[0]) == 1;
+      // Right Turn
       if (isRightTurn) {
-        arrowColor = color(0, 0, 256, 180);
-        description.html(
-          "El nuevo punto da vuelta derecha."
-        );
+        if (lastTurn != null && lastTurn != "right") {
+          description.html(
+            "El nuevo punto da vuelta derecha y la anterior fue izquierda, entonces el primer punto de la arista actual forma parte de una tangente."
+          );
+          tangent_a.push(p.points[0]);
+          tangent_a.push(curr_edge.points[0]);
+          index_a = (curr_index + (convexHull.size() - 1)) % convexHull.size();
+        }
+        else {
+          description.html(
+            "El nuevo punto da vuelta derecha."
+          );
+        }
+        arrowColor = rightTurnColor;
+        lastTurn = "right";
       }
-      else { 
-        arrowColor = color(0, 0, 0, 180);
-        description.html(
-          "El nuevo punto no da vuelta derecha."
-        );
+      // Left Turn
+      else {
+        if (lastTurn != null && lastTurn == "right") {
+          description.html(
+            "El nuevo punto da vuelta izquierda y la anterior fue derecha, entonces el primer punto de la arista actual forma parte de una tangente."
+          );
+          tangent_b.push(p.points[0]);
+          tangent_b.push(curr_edge.points[0]);
+          index_b = (curr_index + (convexHull.size() - 1)) % convexHull.size();
+        }
+        else {
+          description.html(
+            "El nuevo punto da vuelta izquierda."
+          );
+        }
+        arrowColor = leftTurnColor;
+        lastTurn = "left";
       }
     }
-    else{
+    // We know current turn direction, we have to erase it in order to get the next one 
+    else {
       curr_edge.points = [];
       isRightTurn = null;
-
+      arrowColor = null;
     }
   }
-  
 
-  if (curr_edge.size() != 0){
+
+  if (curr_edge.size() != 0) {
     curr_edge_points = curr_edge.toString().split(" ");
     inputDescription.html(
       "Arista actual: (" +
-        curr_edge_points[0] +
-        "," +
-        curr_edge_points[1] + ")"
+      curr_edge_points[0] +
+      "," +
+      curr_edge_points[1] + ")"
     );
   }
 }
@@ -146,17 +222,17 @@ function sleep(millisecondsDuration) {
 
 function draw() {
   background(0);
-  drawCHs();
-  drawP();
   drawTangents();
+  drawCHs();
+  drawPAndTurn();
 }
 
 function drawTangents() {
-  if (tangent_a.size() != 0){
-    tangent_a.drawLinesBetweenPoints(false, "yellow", 2);
+  if (tangent_a.size() != 0) {
+    tangent_a.drawLinesBetweenPoints(false, "yellow", 3);
   }
   if (tangent_b.size() != 0) {
-    tangent_b.drawLinesBetweenPoints(false, "yellow", 2);
+    tangent_b.drawLinesBetweenPoints(false, "yellow", 3);
   }
 }
 
@@ -165,11 +241,11 @@ function drawCHs() {
   convexHull.drawPoints("blue");
 }
 
-function drawP(){
-  if (arrowColor != null){
-    drawDirectedLine(curr_edge.points[0],curr_edge.points[1],color(256, 12, 23, 180));
-    drawArrow(curr_edge.points[1],p.points[0],arrowColor)
-  }
-  curr_edge.drawPoints("red");
+function drawPAndTurn() {
   p.drawPoints("orange");
+  curr_edge.drawPoints("red");
+  if (arrowColor != null) {
+    drawDirectedLine(curr_edge.points[0], curr_edge.points[1], color(256, 12, 23, 180));
+    drawArrow(curr_edge.points[1], p.points[0], arrowColor)
+  }
 }
